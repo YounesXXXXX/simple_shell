4800ef1e0527d43196fde4e0cd81f6aa4635772a
Younes Ait Lahsen <144718552+YounesXXXXX@users.noreply.github.com>
2023-12-13 09:33:31 +0100

    Add memory.c

---
+++ memory.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * xbfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int xbfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
